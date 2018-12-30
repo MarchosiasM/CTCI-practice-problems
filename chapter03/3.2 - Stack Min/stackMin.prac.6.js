65ead4eed3bac4366a6946edaed96d71e83d23c4
Malach, Michael <michael.malach@thermofisher.com>
2018-12-30 12:01:58 -0800

    Didn't pay great attention to which stack I was evaluating from, but overall wasn't too challenging

---
+++ chapter03/3.2 - Stack Min/stackMin.prac.6.js
@@ -0,0 +1,41 @@
+const Stack = require('../util/Stack')
+
+class StackMin {
+    constructor() {
+        this.stack = new Stack();
+        this.minStack = new Stack();
+        this.currMin = undefined;
+    }
+
+    push(value) {
+        this.stack.push(value);
+        if (value <= this.currMin || this.currMin == undefined) {
+            console.log('Adding ', value, ' to the minstack');
+            this.currMin = value;
+            this.minStack.push(value);
+        }
+    }
+
+    pop() {
+        if (this.currMin === this.stack.peek()) {
+            console.log('Popping from minstack ', this.minStack.peek());
+            this.minStack.pop();
+            this.currMin = this.minStack.peek();
+        }
+        return this.stack.pop();
+    }
+
+    min() {
+        return this.currMin;
+    }
+
+    peek() {
+        return this.stack.peek();
+    }
+
+    isEmpty() {
+        return this.stack.isEmpty();
+    }
+}
+
+module.exports = StackMin;
